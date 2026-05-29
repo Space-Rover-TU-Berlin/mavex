19b4242679be450acb8261def4ab2ddbba6c35cc
Koustubh S. <k.sinkar@campus.tu-berlin.de>
2026-05-29 20:41:37 +0200

    Introduce MATLAB

---
+++ src/matlab/literate-prgg.m
@@ -0,0 +1,20 @@
+%[text] This is Matlab's foray into Literate programming
+%[text] This is the matlab equivalent of a Orgmode Babel
+a = 2 %[output:7dc77a95]
+b = 3 %[output:6e7ebe49]
+a + b %[output:2d461454]
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"onright"}
+%---
+%[output:7dc77a95]
+%   data: {"dataType":"textualVariable","outputData":{"name":"a","value":"2"}}
+%---
+%[output:6e7ebe49]
+%   data: {"dataType":"textualVariable","outputData":{"name":"b","value":"3"}}
+%---
+%[output:2d461454]
+%   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"5"}}
+%---
